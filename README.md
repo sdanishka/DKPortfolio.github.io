5c4a6f3357f103e45cdbc1a028963b02d547499b
Danish Afridi <76814231+sdanishka@users.noreply.github.com>
2023-09-02 01:45:59 +0500

    Add files

---
+++ README.md
@@ -0,0 +1,90 @@
+<a name="readme-top"></a>
+
+<!-- PROJECT DESCRIPTION -->
+
+# 📖 Portfolio <a name="about-project"></a>
+
+With this project, you'll be able to showcase your work in a beautiful, fully functional portfolio website. 
+
+<div align="center"><img src='./assets/mock.png' width="700"></div>
+
+## 🛠 Built With <a name="built-with"></a>
+
+### Tech Stack <a name="tech-stack"></a>
+
+- HTML
+- CSS
+- JavaScript
+
+<details>
+<summary>Client</summary>
+</details>
+
+<!-- Features -->
+
+### Key Features <a name="key-features"></a>
+
+- Built using only HTML, CSS, JavaScript
+- You can easily create your portfolio using this repository.
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- GETTING STARTED -->
+
+## 💻 Getting Started <a name="getting-started"></a>
+
+To get a local copy up and running, follow these steps.
+
+### Prerequisites
+
+- Live Server
+
+### Setup
+
+#### Github
+- Enter this url: [https://github.com/sdanishka](https://https://github.com/sdanishka) in your web browser.
+-
+
+### Usage
+
+To run the project:
+
+- Open `index.html` on live server.
+
+### Run tests
+
+To run tests, run the following command:
+
+```
+npm run test
+```
+
+### Deployment
+
+You can deploy this project using:
+
+- Github Pages
+- Netlify
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- ACKNOWLEDGEMENTS -->
+
+## 🙏 Acknowledgments <a name="acknowledgements"></a>
+
+- A big thanks to [@microverseinc](https://github.com/microverseinc) for the project idea, guidance and inspiration.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- FAQ  -->
+
+## ❓ FAQ <a name="faq"></a>
+
+- **What inspired me to create this project?**
+
+  - The inspiration for this project came from microverse – a software development bootcamp where you work with teammates remotely on different projects.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
---
+++ index.js
@@ -0,0 +1,275 @@
+/* eslint-disable linebreak-style */
+const hamburger = document.getElementById('humburger');
+const closeBtn = document.getElementById('close');
+const navbar = document.querySelector('#menu-Elements');
+const menuItems = document.querySelectorAll('.menu-item');
+
+// Adding Projects Dynamically
+const projectsContainer = document.querySelector('.card-holder');
+
+// Adding Modal JavaScript
+const appearMobileModel = document.getElementById('modal'); // Target using ID
+const appearDeskModel = document.getElementById('desk-modal'); // Target using ID
+const MobileModal = document.querySelector('.modal'); // Target using Class
+const deskModal = document.querySelector('.desk-modal'); // Target using ID
+const closeModal = document.querySelector('#close-mobile-btn'); // Target using ID
+const closeModalDSK = document.querySelector('#closebtnDSK'); // Target using ID
+const MobOverlay = document.querySelector('#overlay');
+const Deskoverlay = document.querySelector('#Deskoverlay');
+const mediaQuery = window.matchMedia('(min-width: 768px)');
+
+// Form Validation
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const error = document.getElementById('errors');
+
+// Passing Data to LocalStorage
+let formObj = {};
+
+function openMenu() {
+  navbar.style.display = 'flex';
+  hamburger.style.display = 'none';
+  closeBtn.style.display = 'flex';
+}
+
+hamburger.addEventListener('click', openMenu);
+
+function close() {
+  navbar.style.display = 'none';
+  hamburger.style.display = 'flex';
+  closeBtn.style.display = 'none';
+}
+
+// Close the Mobile Menu
+closeBtn.addEventListener('click', close);
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener('click', () => {
+    if (!mediaQuery.matches) {
+      console.log('yes');
+      navbar.style.display = 'none';
+      hamburger.style.display = 'flex';
+      closeBtn.style.display = 'none';
+    }
+  });
+});
+
+// Generates the Projects in DOM
+function generatProject({ name, image, technologies }, index) {
+  return `
+  <div class="card-holder-item">
+    <div class="holder-image projectImg">
+    <img
+                src=${image}
+                alt='Project img'
+                height="150"
+    />
+    </div>
+      <div class="holder-title">
+          <h3>${name}</h3>
+      </div>
+      
+      <div class="holder-discription">
+        <ul class="tag">
+          ${technologies
+            .map((technology) => `<li class="tag-item">${technology}</li>`)
+            .join('')}
+        </ul>
+      </div>
+      <button class="btn" id="modal-btn-${index}" onclick="handleSeeProject(this)">See Project</button>
+  </div>
+</div>
+  `;
+}
+
+// Object to Pass For generating Projects in DOM
+const projects = [
+  {
+    name: 'AutoVision',
+    description:
+      'An Automatic Machine Learning Tool for Computer vision tasks',
+    image: './assets/Projects/mock1.png',
+    technologies: ['NextJS', 'Python', 'NodeJS'],
+    liveVersion: 'https://spacex-travellers-hub.herokuapp.com/',
+    source: 'https://github.com/developerwaleed/SpaceX-Traveller-Hub',
+  },
+ 
+  {
+    name: 'Model-Master',
+    description:
+      'tracking system for a fast moving object.',
+    image: './assets/Projects/mock3.png',
+    technologies: ['Bootstrap', 'Python', 'JavaScript'],
+    liveVersion: 'https://developerwaleed.github.io/Book-Store_Website-V2/',
+    source: 'https://github.com/developerwaleed/Book-Store_Website-V2',
+  },
+];
+
+function closeModalMobile() {
+  appearMobileModel.style.display = 'none';
+  MobileModal.classList.remove('active');
+  MobOverlay.classList.remove('active');
+}
+
+function closeModalDesktop() {
+  appearDeskModel.style.display = 'none';
+  deskModal.classList.remove('active');
+  Deskoverlay.classList.remove('active');
+}
+
+function handleSeeProject(e) {
+  const name = e.parentElement.children[1].children[0].innerText;
+  projects.forEach((elem) => {
+    if (elem.name === name) {
+      if (mediaQuery.matches) {
+        const modal = `
+            <div class="deskmodal-header">
+                <button type="button" class="deskclosebtn" id="closebtnDSK" onclick="closeModalDesktop()">
+                   &times;
+                </button>
+              <div class="imageDetails">
+                <img
+                  src=${elem.image}
+                  alt="DeskModalImage"
+                  width="450"
+                />
+              </div>
+            </div>
+
+              <div class="Desk-modal-title">
+                <h3>${elem.name}</h3>
+                <div class="Desk-modal-btn-div">
+                <a href="${elem.liveVersion}" target="_blank">
+                  <button type="button" class="Deskmodal-btn"">
+                    <span>See Live</span>
+                    <img src="./assets/icons/model-btn-icon1.svg" alt="social icon" />
+                  </button> 
+                  </a>
+                  <a href="${elem.source}" target="_blank">
+                  <button type="button" class="Deskmodal-btn"">
+                    <span>See Source</span>
+                    <img src="./assets/icons/model-btn-icon2.svg" alt="social icon2" />
+                  </button>
+                  </a>
+                </div>
+              </div>
+              <div class="Desk-modal-body">
+                <ul class="tag">
+                ${elem.technologies
+                  .map(
+                    (technology) => `<li class="tag-item">${technology}</li>`
+                  )
+                  .join('')}
+                </ul>
+                <div class="Desk-modal-discription">
+                  <p>
+                      ${elem.description}
+                  </p>
+                </div>
+              </div>
+                      `;
+        deskModal.innerHTML = modal;
+      }
+    else {
+      const modal = `<div class="modal-header">
+      <img src=${elem.image} alt="image" width = "250"/>
+      <button type="button" class="closebtn" id="close-mobile-btn" onclick="closeModalMobile()">
+        &times;
+      </button>
+    </div>
+
+    <div class="modal-title">
+      <h3>${elem.name}</h3>
+    </div>
+    <div class="modal-body">
+      <ul class="tag">
+      ${elem.technologies
+        .map((technology) => `<li class="tag-item">${technology}</li>`)
+        .join('')}
+      </ul>
+      <div class="modal-discription">
+        <p>
+        ${elem.description}
+        </p>
+      </div>
+    </div>
+    <div class="modal-btn-div">
+      <button type="button" class="modal-btn">
+        <span>See Live</span>
+        <img src="./assets/icons/model-btn-icon1.svg" alt="social icon" />
+      </button>
+      <button type="button" class="modal-btn">
+        <span>See Source</span>
+        <img src="./assets/icons/model-btn-icon2.svg" alt="social icon2" />
+      </button>
+    </div>`;
+      appearMobileModel.innerHTML = modal;
+    }
+  }
+  });
+}
+
+const htmlProjects = projects
+  .map((project, index) => generatProject(project, index))
+  .join('');
+
+projectsContainer.innerHTML = htmlProjects;
+
+// Iterates to input all the projects
+projects.forEach((project, index) => {
+  document
+    .getElementById(`modal-btn-${index}`)
+    .addEventListener('click', () => {
+      if (mediaQuery.matches) {
+        appearDeskModel.style.display = 'flex';
+        deskModal.classList.add('active');
+        Deskoverlay.classList.add('active');
+        body.style.overflow = 'hidden';
+      } else {
+        appearMobileModel.style.display = 'flex';
+        MobileModal.classList.add('active');
+        MobOverlay.classList.add('active');
+      }
+    });
+});
+
+// Code to Validate Form
+form.addEventListener('submit', (event) => {
+  let validation = false;
+  if (email.value === email.value.toLowerCase()) {
+    validation = true;
+  }
+  if (validation === false) {
+    event.preventDefault();
+    error.innerText = 'Email must be in Lower Case!';
+  }
+});
+
+// Populate LocalStrorage
+function populateStorage() {
+  formObj = {
+    Name: form.user.value,
+    Email: form.email.value,
+    Message: form.message.value,
+  };
+  const convertObj = JSON.stringify(formObj);
+  localStorage.setItem('FormData', convertObj);
+}
+
+// Populate FormFields
+function getDataFromLocalStorage() {
+  let data = null;
+  data = localStorage.getItem('FormData');
+  data = JSON.parse(data);
+  form.user.value = data.Name;
+  form.email.value = data.Email;
+  form.message.value = data.Message;
+}
+
+window.onload = () => {
+  form.user.addEventListener('input', populateStorage);
+  form.email.addEventListener('input', populateStorage);
+  form.message.addEventListener('input', populateStorage);
+  if (localStorage.getItem('FormData') !== null) {
+    getDataFromLocalStorage();
+  }
+};
